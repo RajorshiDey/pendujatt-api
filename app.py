155de479166dc6a2b4042e03fa797509c0d1ad7f
RajorshiDey <117197255+RajorshiDey@users.noreply.github.com>
2025-10-05 14:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def home():
     return "🎵 Pendujatt MP3 API is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
